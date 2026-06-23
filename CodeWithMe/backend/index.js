8c0c4ea87b3de6ea1c7c8bc21fc3e5a029f310e7
bhargavi414 <bhargaviroutu224@gmail.com>
2026-06-24 03:56:03 +0530

    test engine development

--- CodeWithMe/backend/index.js
+++ CodeWithMe/backend/index.js
@@ -3,9 +3,16 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 app=express();
-
 app.use(express.json());
 app.use(cors());
+
+const safeDecode = (val) => {
+    try {
+        return val ? atob(val) : "";
+    } catch (e) {
+        return val;
+    }
+};
 
 function auth(request, response, next){
     try{
@@ -65,18 +72,102 @@ app.post("/login", (request, response)=>{
 })
 
 const submissions=[];
-app.post("/submit", (request, response)=>{
+const langMap={
+    cpp : 54,
+    python : 71,
+    java : 63
+}
+
+app.post("/submit", async (request, response)=>{
     submissions.push(
-        {problemId : request.body.problemId, 
-        code : request.body.code, 
-        language : request.body.language
-    })
-    console.log(request.body.problemId)
-    console.log(request.body.code)
-    console.log(request.body.language)
+        {
+            problemId : request.body.problemId, 
+            code : request.body.code, 
+            language : request.body.language,
+        }
+    )
+
+    const lang =request.body.language
+    const source_code = request.body.code
+    const testcases = request.body.testcases
+
+    
+   let passed = 0;
+
+    for (let tc of testcases) {
+
+        const judgeResponse = await fetch(
+            "https://ce.judge0.com/submissions?base64_encoded=false&wait=false", 
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    source_code: source_code,
+                    language_id: langMap[lang],
+                    stdin: tc.input
+                })
+        });
+
+        const result = await judgeResponse.json();
+        const token = result.token;
+
+        let status = "Processing";
+        let codeResult="";
+
+        while (status === "Processing") {
+
+            await new Promise((resolve) =>
+                setTimeout(resolve, 1000)
+            );
+
+            const judge = await fetch(
+                `http://localhost:5713/submission/${token}`,
+                {
+                    method : "GET"
+                }
+            );
+
+            codeResult = await judge.json();
+
+            if (!codeResult.status) {
+                break;
+            }
+        status = codeResult.status.description;
+        }
+
+        const output = safeDecode(codeResult.stdout || "");
+        
+        if (output.trim() === tc.output.trim()) {
+            passed++;
+        }
+    }
+    console.log(passed);
+    const verdict =
+        passed === testcases.length ? "Accepted" : "Wrong Answer";
+
     response.json({
-        success: true
-    })
+        verdict,
+        passed,
+        total: testcases.length
+    });
+
+})
+
+app.get("/submission/:token",async  (request, response)=>{
+    const token = request.params.token;
+    const judge = await fetch(
+        `https://ce.judge0.com/submissions/${token}?base64_encoded=false`,
+        {
+            method : "GET",
+            headers : {
+                "Content-Type": "application/json",
+            }
+        }
+    )
+    const result = await judge.json();
+    response.json(result);
 })
 
 app.get("/submissions", (request, response)=>{
--- CodeWithMe/src/pages/Problem.jsx
+++ CodeWithMe/src/pages/Problem.jsx
@@ -4,25 +4,91 @@ import { useState } from "react";
 export default function Problem(){
     const [code, setCode] = useState("");
     const {id} = useParams();
+    const [exec, setExec]=useState("");
     const problems = [{
         id: 1,
         title: "Two Sum",
         difficulty: "Easy",
         statement: "Given an array and a target number find the two indices whose sum is equal to target",
-        example: "nums=[2,7,11,15]"
+        testcases : [
+            {
+                input : "4 9 2 7 11 15",
+                output : "1 2"
+            },
+            {
+                input : "5 3 4 2 1 5",
+                output : "2 3"
+            }
+        ]
     },
     {   
         id: 2,
-        title: "Reverse Linked List",
+        title: "Reverse Array",
         difficulty: "Easy",
-        statement: "Reverse the given linked list",
-        example: "[1,2,3,4]"
+        statement: "Reverse the given array, 1st line contains number of elements n, following n lines contains elements",
+        testcases : [
+            {
+                input : "4 2 7 11 15",
+                output : "15 11 7 2"
+            },
+            {
+                input : "5 3 4 2 1 5",
+                output : "5 1 2 4 3"
+            }
+        ]
     }
     ]
 
     const selectedProblem=problems.find((problem)=>{
         return problem.id== id
     });
+
+    const safeDecode = (val) => {
+        try {
+            return val ? atob(val) : "";
+        } catch (e) {
+            return val;
+        }
+    };
+
+   let output = null;
+
+    const expected = selectedProblem?.expectedOutput || "";
+    const testcases = selectedProblem.testcases;
+
+    if (exec?.stdout) {
+        const userOutput = safeDecode(exec.stdout).trim();
+        const expectedOutput = expected.trim();
+
+        if (userOutput === expectedOutput) {
+            output = (
+                <div style={{ color: "green" }}>
+                    Accepted ✔
+                    <br />
+                    Output: {userOutput}
+                </div>
+            );
+        } 
+        else {
+            output = (
+                <div style={{ color: "red" }}>
+                    Wrong Answer ❌
+                    <br />
+                    Your Output: {userOutput}
+                    <br />
+                    Expected: {expectedOutput}
+                </div>
+            );
+        }
+    } 
+    else {
+        output = (
+            <div style={{ color: "red" }}>
+                Error (no output returned)
+            </div>
+        );
+    }
+
     return(
         <div style={{ padding: "20px" }}>
             
@@ -40,37 +106,91 @@ export default function Problem(){
             {selectedProblem.example}
             </pre>
 
-            <textarea
+            <textarea 
             placeholder="Write your code here..."
             value={code}
             onChange={(e) => setCode(e.target.value)}
             />
 
             <button
-            onClick={()=>{
-                const problemId=id;
-                const language ="cpp";
-                const submission = {problemId, code, language};
-                async function handler(){
+                onClick={async () => {
+                    console.log("submit clicked")
+                    const problemId = id;
+                    const language = "cpp";
+                    const submission = {
+                        problemId,
+                        code,
+                        language,
+                        testcases
+                    };
+
                     const response = await fetch(
                         "http://localhost:5713/submit",
-                        {   method : "POST",
-                            body : JSON.stringify(submission),
-                            headers : {
+                        {
+                            method: "POST",
+                            body: JSON.stringify(submission),
+                            headers: {
                                 "Content-Type": "application/json"
                             }
                         }
-                    )
-                    const result = await response.json();
-                    console.log(result.success);
-                }
-                handler();
+                    );
 
-            }}
-            style={{marginTop: "20px"}}
-            className="bg-gray-500 font-bold  p-1 rounded-lg">
-                Submit
+                    const result = await response.json();
+                    const token = result.token;
+
+                    let status = "Processing";
+                    let codeResult;
+
+                    while (status === "Processing") {
+
+                        await new Promise((resolve) =>
+                            setTimeout(resolve, 1000)
+                        );
+
+                        const judge = await fetch(
+                            `http://localhost:5713/submission/${token}`
+                        );
+
+                        codeResult = await judge.json();
+
+                        if (!codeResult.status) {
+                            setExec("Invalid code");
+                            return;
+                        }
+                    status = codeResult.status.description;
+                    }
+                    setExec(codeResult);
+                }}
+
+                style={{ marginTop: "20px" }}
+                className="bg-gray-500 font-bold p-1 rounded-lg"
+                >
+                    Submit
             </button>
+            <div>{output}</div>
         </div>
     )
 }
+
+
+   // if (!exec) {
+    //     output = <div>Processing...</div>;
+    // }
+
+    // else if (exec?.stdout) {
+    //     output = (
+    //         <div style={{ color: "green" }}>
+    //             Accepted ✔
+    //             <br />
+    //             Output: {safeDecode(exec.stdout)}
+    //         </div>
+    //     );
+    // }
+
+    // else {
+    //     output = (
+    //         <div style={{ color: "red" }}>
+    //             Error
+    //         </div>
+    //     );
+    // }
